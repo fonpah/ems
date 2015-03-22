65b40760f0a06b4da880b288a0b8521f987c0e86
fon-pah <fonpah@yahoo.fr>
2015-03-22 01:48:05 +0100

    work in  progress

---
+++ api/src/AppBundle/Controller/ContractController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fonpah
+ * Date: 21.03.2015
+ * Time: 20:46
+ */
+
+namespace AppBundle\Controller;
+
+
+class ContractController {
+
+}
---
+++ api/src/AppBundle/Entity/Project.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fonpah
+ * Date: 21.03.2015
+ * Time: 20:41
+ */
+
+namespace AppBundle\Entity;
+
+
+class Project {
+
+}
